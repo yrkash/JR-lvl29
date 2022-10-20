3fd638003fc948e7fc2307cbb079d7d1e39f58d1
yrkash <yrkash@gmail.com>
2022-10-20 17:35:43 +0300

    Added Waiter.java

--- src/com/javarush/task/task27/task2712/Restaurant.java
+++ src/com/javarush/task/task27/task2712/Restaurant.java
@@ -3,12 +3,14 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.kitchen.Dish;
 import com.javarush.task.task27.task2712.kitchen.Order;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 public class Restaurant {
     public static void main(String[] args) {
         Cook cook = new Cook("Irina");
         Tablet tablet = new Tablet(5);
         tablet.addObserver(cook);
+        cook.addObserver(new Waiter());
         tablet.createOrder();
         tablet.createOrder();
         tablet.createOrder();
