c398a60b24b30a771114f538ab874d424cf558d5
yrkash <yrkash@gmail.com>
2022-10-24 18:25:31 +0300

    Added package statistic with subpackage event

---
+++ src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task27.task2712.statistic;
+
+import com.javarush.task.task27.task2712.ad.AdvertisementStorage;
+import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StatisticManager {
+    private static StatisticManager instance;
+    private StatisticStorage statisticStorage = new StatisticStorage();
+    private StatisticManager() {
+    }
+
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            for (EventType eventType: EventType.values()) {
+                storage.put(eventType, new ArrayList<EventDataRow>());
+            }
+        }
+    }
+
+    public static StatisticManager getInstance() {
+        if (instance == null) {
+            instance = new StatisticManager();
+        }
+        return instance;
+    }
+
+    public void register(EventDataRow data) {
+
+    }
+
+}
